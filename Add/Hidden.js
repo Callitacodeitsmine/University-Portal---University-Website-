ed00162bec1e2ee81e195de4e1c60454208e9cdf
Callitacodeitsmine <107363591+Callitacodeitsmine@users.noreply.github.com>
2022-09-20 10:44:12 +0530

    Delete Add directory

--- Add/Hidden.js
+++
@@ -1,9 +0,0 @@
-<script>
-    var navLinks = document.getElementById("navLinks");
-    function showMenu(){
-      navLinks.style.right = "0"
-    }
-    function showMenu(){
-      navLinks.style.right = "-200px"
-    }
-  </script>
