810b431ebd4eb38ad100ed5b5d4859aeed66c35d
Callitacodeitsmine <107363591+Callitacodeitsmine@users.noreply.github.com>
2022-09-20 10:53:39 +0530

    Add files via upload

---
+++ Add/Hidden.js
@@ -0,0 +1,9 @@
+<script>
+    var navLinks = document.getElementById("navLinks");
+    function showMenu(){
+      navLinks.style.right = "0"
+    }
+    function showMenu(){
+      navLinks.style.right = "-200px"
+    }
+  </script>
